b1b6803588a7db2fc204f87a71b95a821671f412
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-09-22 18:15:47 -0300

    Add alterações para conseguir testar o Hibernate

--- financas/src/br/com/gile/financas/teste/TesteConta.java
+++ financas/src/br/com/gile/financas/teste/TesteConta.java
@@ -1,5 +1,9 @@
 package br.com.gile.financas.teste;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import br.com.gile.financas.modelo.Conta;
 
 public class TesteConta {
@@ -10,8 +14,14 @@ public class TesteConta {
 		conta.setBanco("Nubank");
 		conta.setNumero("456");
 		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
+		EntityManager em = emf.createEntityManager();
 		
+		em.getTransaction().begin();
+		em.persist(conta);
+		em.getTransaction().commit();
 		
-		
+		em.close();
+		emf.close();
 	}
 }
