209758efba37135c77efafde74b80f5eb6c624f5
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-09-23 18:14:20 -0300

    Add classe de Movimentação

---
+++ financas/src/main/java/br/com/gile/financas/modelo/Movimentacao.java
@@ -0,0 +1,76 @@
+package br.com.gile.financas.modelo;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Movimentacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private BigDecimal valor;
+	
+	//Quando é criado um ENUM, é assim que se passa para a JPA
+	@Enumerated(EnumType.STRING)
+	private TipoMovimentacao tipoMovimentacao;
+	
+	//Quando se trata de data, pode ser passado DATE / TIME / TIMESTAMP
+	//DATE pega somente a data
+	//TIME pega somente a Hora
+	//TIMESTAMP pega data e hora
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar data;
+	private String descricao;
+	
+	//Varias Movimentações para uma conta
+	@ManyToOne
+	private Conta conta;
+	
+	public Conta getConta() {
+		return conta;
+	}
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public TipoMovimentacao getTipoMovimentacao() {
+		return tipoMovimentacao;
+	}
+	public void setTipoMovimentacao(TipoMovimentacao tipoMovimentacao) {
+		this.tipoMovimentacao = tipoMovimentacao;
+	}
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
---
+++ financas/src/main/java/br/com/gile/financas/modelo/TipoMovimentacao.java
@@ -0,0 +1,6 @@
+package br.com.gile.financas.modelo;
+
+public enum TipoMovimentacao {
+
+	ENTRADA,SAIDA
+}
